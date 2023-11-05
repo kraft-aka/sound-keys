af3554938ccd6fee7b8278ece3982bb10fe48e3a
kubat <kubat.aiupov@gmail.com>
2023-11-05 22:26:38 +0100

    add removeTransition

--- index.js
+++ index.js
@@ -4,9 +4,17 @@ const playSound = (e) => {
   if (!audio) return;
   audio.play();
   audio.currentTime = 0;
-  key.classList.add('playing')
+  key.classList.add("playing");
   console.log(key);
 };
 
 window.addEventListener("keydown", playSound);
 
+const keys = document.querySelectorAll(".key");
+
+const removeTransition = (e) => {
+  if (!e.propertyName === "transform") return;
+  e.target.classList.remove("playing");
+  console.log(e);
+};
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
